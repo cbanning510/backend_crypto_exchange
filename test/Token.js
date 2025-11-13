197e2d68e984771420695b0eea89cecbb0a07401
Chris Banning <chrisbanning510@gmail.com>
2025-11-12 22:25:15 -0500

    3. Testing Contracts

---
+++ test/Token.js
@@ -0,0 +1,27 @@
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { deployTokenFixture } = require("./helpers/TokenFixtures");
+
+const tokens = (n) => {
+  return ethers.parseUnits(n.toString(), 18);
+};
+
+describe("Token\n", () => {
+  it("has correct name", async () => {
+    const { token } = await loadFixture(deployTokenFixture);
+    expect(await token.name()).to.equal("Dapp University");
+  });
+  it("has correct name", async () => {
+    const { token } = await loadFixture(deployTokenFixture);
+    expect(await token.symbol()).to.equal("DAPP");
+  });
+  it("has correct number of decimals", async () => {
+    const { token } = await loadFixture(deployTokenFixture);
+    expect(await token.decimals()).to.equal(18);
+  });
+  it("has correct total supply", async () => {
+    const { token } = await loadFixture(deployTokenFixture);
+    expect(await token.totalSupply()).to.equal(tokens("1000000"));
+  });
+});
